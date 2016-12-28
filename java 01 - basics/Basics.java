ca5147e2fa4dc10c471f2ddd4eac63823263d816
Barrett Amsrud <barrettamsrud@gmail.com>
2016-12-28 15:38:35 -0600

    Changed the firstName variable to take user input

--- java 01 - basics/Basics.java
+++ java 01 - basics/Basics.java
@@ -5,7 +5,7 @@ public class Basics {
   public static void main(String[] args) {
     Console console = System.console();
     //  Hold a name in a variable, firstName, as a String
-    String firstName = "Barrett";
+    String firstName = console.readLine("What is your name?  ");
     //  Insert the firstName variable into the string
     console.printf("Hello, my name is %s\n", firstName);
     console.printf("Today's date is December 28th, 2016\n");
