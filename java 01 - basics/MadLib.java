2bd99b9782e339bec392d83d583f913a95bbc977
Barrett Amsrud <barrettamsrud@gmail.com>
2016-12-29 15:15:13 -0600

    Expanded MadLib.java

--- java 01 - basics/MadLib.java
+++ java 01 - basics/MadLib.java
@@ -12,7 +12,13 @@ public class MadLib {
         */
         String name = console.readLine("Enter your name:  ");
         String adjective = console.readLine("Enter an adjective:  ");
-        console.printf("%s is very %s", name, adjective);
+        String noun = console.readLine("Enter a noun:  ");
+        String adverb = console.readLine("Enter an adverb:  ");
+        String verb = console.readLine("Enter a verb ending in -ing:  ");
+
+        console.printf("Your Story\n \n");
+        console.printf("%s is a %s %s.  ", name, adjective, noun);
+        console.printf("They are always %s %s. \n", adverb, verb);
     }
 
 }
