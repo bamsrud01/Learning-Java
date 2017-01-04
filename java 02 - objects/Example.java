832964a63f3524566a593018a8f5778d6c5e98f2
Barrett Amsrud <barrettamsrud@gmail.com>
2017-01-04 15:53:18 -0600

    Added constant to hold maximum amount of PEZ

--- java 02 - objects/Example.java
+++ java 02 - objects/Example.java
@@ -3,9 +3,11 @@ public class Example {
   public static void main(String[] args) {
     //  Create a PEZ dispenser object
     System.out.println("We are making a PEZ Dispenser");
+    //  Use the constant value of max PEZ
+    System.out.printf("There are %d PEZ allowed in every dispenser %n", PezDispenser.MAX_PEZ);
     //  Create a new PEZ dispenser object
-    PezDispenser dispenser = new PezDispenser();
-    System.out.printf("This dispenser is %s %n", dispenser.getCharacterName("Yoda"));
+    PezDispenser dispenser = new PezDispenser("Yoda");
+    System.out.printf("This dispenser is %s %n", dispenser.getCharacterName());
     //  Change the characterName property on this instance of the PezDispenser object
 
   }
--- java 02 - objects/PezDispenser.java
+++ java 02 - objects/PezDispenser.java
@@ -1,6 +1,10 @@
 class PezDispenser {
   //  Inside the class scope
 
+  //  Constants generally are written in all caps.
+  //  The final keyword prevents it from being assigned a new value
+  public static final int MAX_PEZ = 12;
+
   //  The keyword 'private' here ensures that other files in the same package cannot access the variable
   private String characterName;
 
