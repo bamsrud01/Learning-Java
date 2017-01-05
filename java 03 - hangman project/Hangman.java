98743f73755c9c5567d38fdbc4f068e9fa5cc216
Barrett Amsrud <barrettamsrud@gmail.com>
2017-01-05 09:33:33 -0600

    Added code to determine if user's guess is in the answer

--- java 03 - hangman project/Hangman.java
+++ java 03 - hangman project/Hangman.java
@@ -2,7 +2,17 @@ public class Hangman {
 
   public static void main(String[] args) {
 
+    //  Create an instance of Game object with the answer of "treehouse"
     Game game = new Game("treehouse");
+    //  Create an instance of Prompter object using the game we have created
+    Prompter prompter = new Prompter(game);
+
+    boolean isHit = prompter.promptForGuess();
+    if (isHit) {
+      System.out.println("We got a hit!");
+    } else {
+      System.out.println("Not a hit.");
+    }
 
   }
 }
--- java 03 - hangman project/Prompter.java
+++ java 03 - hangman project/Prompter.java
@@ -1,1 +1,25 @@
-class Prompter {}
+//  Use Scanner to take user input
+import java.util.Scanner;
+
+class Prompter {
+
+  //  Create instance of Game object
+  private Game game;
+
+  //  constructor for Prompter objects
+  public Prompter(Game game) {
+    this.game = game;
+  }
+
+  //  Handle user input for guesses
+  public boolean promptForGuess() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a letter:  ");
+    String guessInput = scanner.nextLine();
+    //  Take only the character at index 0
+    char guess = guessInput.charAt(0);
+    //  Determine if character at index 0 is in the Hangman word
+    return game.applyGuess(guess);
+  }
+
+}
