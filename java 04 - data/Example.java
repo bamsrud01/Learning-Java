ca3ae50ac5a7c97af5ca9f58588df5b71b5925b4
Barrett Amsrud <barrettamsrud@gmail.com>
2017-01-16 18:51:03 -0600

    Added constructor and getters to the Treet class

--- java 04 - data/Example.java
+++ java 04 - data/Example.java
@@ -1,10 +1,15 @@
-//  First, import our package
+//  First, import our package and Date
+import java.util.Date;
 import com.teamtreehouse.Treet;
 
 public class Example {
 
   public static void main(String[] args) {
-    Treet treet = new Treet();
+    Treet treet = new Treet(
+      "Barrett",
+      "This is my Treet: A Treehouse Tweet",
+      new Date(1421849732000L)  //  The L indicates the integer is long
+    );
     System.out.printf("This is a new treet: %s %n", treet);
   }
 
--- java 04 - data/com/teamtreehouse/Treet.java
+++ java 04 - data/com/teamtreehouse/Treet.java
@@ -5,7 +5,32 @@
 //  Declare file as part of the package
 package com.teamtreehouse;
 
+//  Import Date
+import java.util.Date;
+
 //  Declare class
 public class Treet {
+  private String mAuthor;
+  private String mDescription;
+  private Date mCreationDate;
+
+  public Treet(String author, String description, Date creationDate) {
+    mAuthor = author;
+    mDescription = description;
+    mCreationDate = creationDate;
+  }
+
+  //  Getter methods
   
+  public String getAuthor() {
+    return mAuthor;
+  }
+
+  public String getDescription() {
+    return mDescription;
+  }
+
+  public Date getCreationDate() {
+    return mCreationDate;
+  }
 }
