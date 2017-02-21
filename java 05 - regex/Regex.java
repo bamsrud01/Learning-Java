3d1c15c0dc9ef556ac1cdfc37a0fbb69acdbf38e
Barrett Amsrud <barrettamsrud@gmail.com>
2017-02-20 20:21:47 -0600

    Added more patterns to regex

--- java 05 - regex/Regex.java
+++ java 05 - regex/Regex.java
@@ -8,11 +8,27 @@ class Regex {
 
     //  Code starting here
 
-    String zipCode = console.readLine("Enter your zip code: ");
+    String zipCode = "55414";
     if (zipCode.matches("^\\d{5}(-\\d{4})?$")) {
       System.out.printf("%s is a valid zip code!%n", zipCode);
     } else {
       System.out.printf("%s is not a valid zip code.%n", zipCode);
+    }
+
+    String skills = "JavaScript, HTML, CSS, and Java";
+    for (String skill : skills.split("\\W+(and\\W+)?")) {
+      System.out.printf("Skill: %s %n", skill);
+    }
+    String script = "Surely capturing shushes shall show how beneficial Regular Expressions actually are";
+    //  Use Pattern
+    Pattern pattern = Pattern.compile("(\\w*(sh|ti|su|ci|tu|si)\\w*)", Pattern.CASE_INSENSITIVE);
+    //  User Matcher
+    Matcher matcher = pattern.matcher(script);
+    while (matcher.find()) {
+      System.out.printf("%s is a shushy word because of %s %n",
+        matcher.group(1),
+        matcher.group(2));
+        //  The parantheses around the regex on line 24 indicate how far matcher.group() must go
     }
   }
 }
